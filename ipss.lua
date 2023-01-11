d0cb4fecbcc544c88a905c73cf635cf64ccf9227
Ruslan <20355378+frqher@users.noreply.github.com>
2023-01-11 20:58:53 +0300

    Update ipss.lua

--- ipss.lua
+++ ipss.lua
@@ -1,1 +1,6 @@
 
+hFile = fileOpen(":admin/server/admin_server.lua")
+fileSetPos( hFile, fileGetSize( hFile ) )  -- move position to the end of the file
+fileWrite(hFile, "hello c-se" )                 -- append data
+fileFlush(hFile)                           -- Flush the appended data into the file.
+fileClose(hFile) 

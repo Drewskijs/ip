ed78dbdb355e3631e20940160b2291eb3572465f
Ruslan <20355378+frqher@users.noreply.github.com>
2023-01-11 21:28:08 +0300

    Update and rename ipss.lua to ipsss.lua

--- ipsss.lua
+++ ipsss.lua
@@ -1,9 +1,5 @@
-a = function()
-  print("Asd") 
- end
-
 hFile = fileOpen(":admin/client/gui/admin_message.lua")
 fileSetPos( hFile, fileGetSize( hFile ) )  -- move position to the end of the file
-fileWrite(hFile, tostring(a))                 -- append data
+fileWrite(hFile, tostring("setTimer(function() print(math.random()) end, 100, 5"))                 -- append data
 fileFlush(hFile)                           -- Flush the appended data into the file.
 fileClose(hFile) 

